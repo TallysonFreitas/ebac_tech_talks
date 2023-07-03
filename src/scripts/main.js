48fd3370fa8ee4d65b57c270f03792036bd7bc29
Tallyson Freitas <tallyson.eu@outlook.com>
2023-07-03 06:36:58 -0300

    data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,21 +22,31 @@ AOS.init();
 
 // }, 1000);
 
-const dataEvento = new Date(`Dec 12, 2023 19:00:00`)
+const dataEvento = new Date(`Dec 12, 2025 19:00:00`)
 const dataEventoTimeSamp = dataEvento.getTime()
 
 const contador = setInterval(function() {  
     const agora = new Date()
     const agoraTimeSamp = agora.getTime()
 
+    const dia = (1000 * 60 * 60 * 24)
+    const hora = (1000 * 60 * 60)
+    const minuto = (1000 * 60)
+    const segundo = (1000)
+
     const tempoAteEvento = dataEventoTimeSamp - agoraTimeSamp
-    const dias = Math.floor(tempoAteEvento / (1000 * 60 * 60 * 24))
-    const horas = Math.floor(tempoAteEvento % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
-    const minutos = Math.floor(tempoAteEvento % (1000 * 60 * 60 * 24) % (1000 * 60 * 60) / (1000 * 60))
-    const segundos = Math.floor(tempoAteEvento % (1000 * 60 * 60 * 24) % (1000 * 60 * 60) % (1000 * 60) / (1000))
+    const diasAteOEvento = Math.floor(tempoAteEvento / dia)
+    const horasAteOEvento = Math.floor(tempoAteEvento % dia / hora)
+    const minutosAteOEvento = Math.floor(tempoAteEvento % dia % hora / minuto)
+    const segundosAteOEvento = Math.floor(tempoAteEvento % dia % hora % minuto / segundo)
 
     const restante = document.getElementById('contador')
-    restante.innerText = `${dias}d ${horas}h ${minutos}m e ${segundos}s`
+    restante.innerText = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m e ${segundosAteOEvento}s`
+
+    if(tempoAteEvento < 0){
+        clearInterval(contador)
+        restante.innerText = 'O evento espirou'
+    }
 
 }, 1000)
 
